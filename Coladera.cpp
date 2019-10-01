d66d1ca1654093c044238a9f28ca9ea023326165
Krovs1 <54084040+Krovs1@users.noreply.github.com>
2019-10-01 17:52:26 -0500

    Update Coladera.cpp

--- Coladera.cpp
+++ Coladera.cpp
@@ -1,5 +1,53 @@
 using namespace std;
 #include<iostream>
 
+//Prototipos
+void LlenarUnos (int vec[],int n);
+void Coladera (int vec[],int n);
+void Mostrar (int vec[],int n);
 
-for(int i=2;i<n;i++)
+int main(){
+	
+	int vec[1000];
+	
+	LlenarUnos(vec,1000);
+	Coladera(vec,1000);
+	Mostrar(vec,1000);
+	
+	
+	
+	
+}
+
+//Funcion que llena el vector de unos
+void LlenarUnos (int vec[],int n){
+	
+for(int i=1;i<n;i++){
+	vec[i]i=1;
+cout<<i<<endl;	
+}	
+}
+
+
+void Coladera (int vec[],int n){
+	
+	for(int=2;i<n;)
+ 		
+}
+
+
+
+
+void Mostrar (int vec[],int n){
+	
+	for(int i=2;i<n;i++){
+	
+	
+	cout<<"Posicion "<<i<<": "<<vec[i]<<endl;
+  	
+}
+
+}
+
+
+
